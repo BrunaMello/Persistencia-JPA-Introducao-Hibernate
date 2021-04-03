16cd78238cc49e006eafdd49139c4d7d925a89e2
brunamello <brunacrespomello@gmail.com>
2021-04-03 21:58:33 +0100

    05.Consultas com JPQL final da primeira parte do curso.

--- src/main/java/dao/ProdutoDAO.java
+++ src/main/java/dao/ProdutoDAO.java
@@ -3,6 +3,8 @@ package dao;
 import modelo.Produto;
 
 import javax.persistence.EntityManager;
+import java.math.BigDecimal;
+import java.util.List;
 
 public class ProdutoDAO {
 
@@ -15,4 +17,37 @@ public class ProdutoDAO {
     public void cadastrar(Produto produto) {
         this.em.persist(produto);
     }
+
+    public Produto buscarPorId(Long id) {
+        return em.find(Produto.class, 1l);
+    }
+
+    public List<Produto> buscarTodos() {
+        String jpql = "SELECT p FROM Produto p";
+        //createquery somente monta a query é preciso o get resulta para disparar para o DB
+        return em.createQuery(jpql, Produto.class).getResultList();
+    }
+
+    public List<Produto> buscarPorNome(String nome) {
+        // no lugar de :nome pode usar o ?1, sepois é so apontar a posicao
+        String jpql = "SELECT p FROM Produto p WHERE p.nome = :nome";
+        return em.createQuery(jpql, Produto.class)
+                .setParameter("nome", nome)
+                .getResultList();
+    }
+
+    public List<Produto> buscarPorNomedaCategoria(String nome) {
+        String jpql = "SELECT p FROM Produto p WHERE p.categoria.nome = :nome";
+        return em.createQuery(jpql, Produto.class)
+                .setParameter("nome", nome)
+                .getResultList();
+    }
+
+    public BigDecimal buscarPrecoDoProdutoComNome(String nome) {
+        String jpql = "SELECT p.preco FROM Produto p WHERE p.nome = :nome";
+        return em.createQuery(jpql, BigDecimal.class)
+                .setParameter("nome", nome)
+                .getSingleResult();
+    }
+
 }
--- src/main/java/testes/CadastroDeProduto.java
+++ src/main/java/testes/CadastroDeProduto.java
@@ -8,22 +8,46 @@ import util.JPAUtil;
 
 import javax.persistence.EntityManager;
 import java.math.BigDecimal;
+import java.util.List;
 
 public class CadastroDeProduto {
 
     public static void main(String[] args) {
 
+        cadastrarProduto();
+
+        //criando um entitymanager para chamar a class jpautil
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDAO produtoDAO = new ProdutoDAO(em);
+
+//        Produto p = produtoDAO.buscarPorId(1l);
+//        System.out.println("Nome do produto: " + p.getNome());
+//        System.out.println("Valor do produto: " +p.getPreco());
+//
+//        List<Produto> todos = produtoDAO.buscarTodos();
+//        todos.forEach(p2 -> System.out.println("Lista de Produtos: " + p2.getNome() + " ID: " + p2.getId()));
+//
+//        List<Produto> buscarNome = produtoDAO.buscarPorNome("Iphone 12pro");
+//        buscarNome.forEach(p3 -> System.out.println("O produto é: " + p3.getNome()));
+
+        List<Produto> buscarNomeDaCategoria = produtoDAO.buscarPorNomedaCategoria("CELULARES");
+        buscarNomeDaCategoria.forEach(p4 -> System.out.println("O produto é: " + p4.getNome()));
+
+        BigDecimal preco = produtoDAO.buscarPrecoDoProdutoComNome("Iphone 12pro");
+        System.out.println("O preco do Produto é: " + preco);
+
+
+    }
+
+    private static void cadastrarProduto() {
         Categoria celulares = new Categoria("CELULARES");
 
 
         Produto celular = new Produto("Iphone 12pro", "256gb", new BigDecimal("1200"), celulares);
 
 
-
-
         //criando um entitymanager para chamar a class jpautil
         EntityManager em = JPAUtil.getEntityManager();
-
         ProdutoDAO produtoDAO = new ProdutoDAO(em);
         CategoriaDAO categoriaDAO = new CategoriaDAO(em);
 
