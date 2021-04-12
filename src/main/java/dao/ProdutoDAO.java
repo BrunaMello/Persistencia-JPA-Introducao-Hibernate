f88d57036e0498f4806fc6e824ab0bd944942aa4
brunamello <brunacrespomello@gmail.com>
2021-04-12 20:35:10 +0100

    04. Criteria API
    Nessa aula, você aprendeu:
    - Como realizar consultas JPQL com parâmetros opcionais;
    - Como funciona a Criteria API da JPA;
    - Como realizar uma consulta com parâmetros opcionais via Criteria API.

--- src/main/java/dao/ProdutoDAO.java
+++ src/main/java/dao/ProdutoDAO.java
@@ -3,7 +3,13 @@ package dao;
 import modelo.Produto;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 public class ProdutoDAO {
@@ -47,6 +53,55 @@ public class ProdutoDAO {
         return em.createQuery(jpql, BigDecimal.class)
                 .setParameter("nome", nome)
                 .getSingleResult();
+    }
+
+    //consultas com parametros nao obrigatorios, dinamicos
+    public List<Produto> buscarPorParametros(String nome, BigDecimal preco, LocalDate dataCadastro) {
+        String jpql = "SELECT p FROM Produto p WHERE 1=1";
+        if (nome != null && !nome.trim().isEmpty()) {
+            jpql = " AND p.nome = :nome";
+        }
+        if (preco != null) {
+            jpql = " AND p.preco = :preco";
+        }
+        if (dataCadastro != null) {
+            jpql = " AND p.dataCadastro = :dataCadastro";
+        }
+
+        TypedQuery<Produto> query = em.createQuery(jpql, Produto.class);
+        if (nome != null && !nome.trim().isEmpty()) {
+            query.setParameter("nome", nome);
+        }
+        if (preco != null) {
+            query.setParameter("preco", preco);
+        }
+        if (dataCadastro != null) {
+            query.setParameter("dataCadastro", dataCadastro);
+        }
+        return query.getResultList();
+
+    }
+
+    //consultas com parametros nao obrigatorios, dinamicos sem codigos duplicados usando o criteria
+    public List<Produto> buscaPorProdutoComCriteria(String nome, BigDecimal preco, LocalDate dataCadastro){
+        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+        CriteriaQuery<Produto> query = criteriaBuilder.createQuery(Produto.class);
+        Root<Produto> from = query.from(Produto.class);
+
+        Predicate filtros = criteriaBuilder.and();
+
+        if (nome != null && !nome.trim().isEmpty()) {
+            filtros = criteriaBuilder.and(filtros, criteriaBuilder.equal(from.get("nomeProduto"), nome));
+        }
+        if (preco != null) {
+            filtros = criteriaBuilder.and(filtros, criteriaBuilder.equal(from.get("preco"), preco));
+        }
+        if (dataCadastro != null) {
+            filtros = criteriaBuilder.and(filtros, criteriaBuilder.equal(from.get("dataCadastro"), dataCadastro));
+        }
+        query.where(filtros);
+
+        return em.createQuery(query).getResultList();
     }
 
 
---
+++ src/main/java/testes/TesteCriteria.java
@@ -0,0 +1,20 @@
+package testes;
+
+import dao.ProdutoDAO;
+import util.JPAUtil;
+
+import javax.persistence.EntityManager;
+import java.time.LocalDate;
+
+public class TesteCriteria {
+
+    public static void main(String[] args) {
+
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDAO produtoDAO = new ProdutoDAO(em);
+
+        produtoDAO.buscaPorProdutoComCriteria(null, null, LocalDate.now());
+
+
+    }
+}
